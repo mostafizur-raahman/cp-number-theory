f3950de1c3bd9806db3206d9304688a917638d15
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-03-24 21:32:29 +0600

    added  sumOfItsAllFactor function

--- prime.cpp
+++ prime.cpp
@@ -28,9 +28,26 @@ bool checkPrimeOrdrOfSQRT(int n){
     if( cnt == 2 ) return 1;
     else return 0;
 }
+
+// print the sum by its factor like n = 5 = dividor(1,5) = sum is 1 + 5 = 6 and n = 6 sum is = 6 + 2 + 3 +1 = 12
+
+int sumOfItsAllFactor(int n){
+    int sum = 0;
+
+    for(int i = 1; i*i <= n; i++){
+        if(n % i == 0){
+            sum += i;
+            if( n/i != i ){
+                sum += (n/i);
+            }
+        }
+    }
+    return sum;
+}
 int main(){
 
     cout << checkPrimeOrderOfN(17) << endl;
-    cout << checkPrimeOrdrOfSQRT(17);
+    cout << checkPrimeOrdrOfSQRT(17) << endl;
+    cout << sumOfItsAllFactor(5) << endl;
 
 }
